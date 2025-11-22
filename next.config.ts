c361798fedadaaed2af049734bdcdac20b8051e9
Felicia <34881756+feliciachang@users.noreply.github.com>
2025-11-21 16:44:46 -0800

    wip

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Optional: don’t fail the build on ESLint errors either
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
